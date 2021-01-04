1fa6ce727542b4684b72017511ff834763adc810
Jonathan Basseri <misterikkit@gmail.com>
2021-01-03 21:59:22 -0800

    bugfix: player view was using admin socket endpoint

--- static/js/game.js
+++ static/js/game.js
@@ -1,7 +1,7 @@
 function init() {
     const urlParams = new URLSearchParams(location.search);
     gameID = urlParams.get('gameID')
-    connect(`ws/admin/${getGameID()}`, render);
+    connect(`ws/player/${getGameID()}`, render);
 }
 
 $(init);
